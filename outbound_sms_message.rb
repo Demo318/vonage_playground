11530959df413fa0fd80f75d3974c4285b3ecf18
Devin Mork <devin.mork@gmail.com>
2021-02-12 15:44:08 -0600

    Add #initialize and #send_sms_message to OutboundSMSMessage class.

--- outbound_sms_message.rb
+++ outbound_sms_message.rb
@@ -4,4 +4,19 @@ require 'json'
 class OutboundSMSMessage
     include NexmoCredentials
 
+    def initialize()
+        @nexmo_client = Nexmo::Client.new(
+            api_key: Key,
+            api_secret: Secret
+        )
+    end
+
+    def send_sms_message(from_number, to_number, message)
+        @nexmo_client.sms.send(
+            from: from_number,
+            to: to_number,
+            text: message
+        )
+    end
+
 end
