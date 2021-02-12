1aa25c00dd4bb9bed8a1b59cff237cc8f3c19dde
Devin Mork <devin.mork@gmail.com>
2021-02-12 09:09:50 -0600

    Add InboundVoiceCall class.

---
+++ inbound_voice_call.rb
@@ -0,0 +1,10 @@
+﻿class InboundVoiceCall
+
+    def initialize(call_to, call_from, call_uuid, call_conversation_uuid)
+        @to_number = call_to
+        @from_number = call_from
+        @uuid = call_uuid
+        @conversation_uuid = call_conversation_uuid
+    end
+
+end
