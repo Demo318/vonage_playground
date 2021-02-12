2c22067c92f49c4890ec14896955cda51129d572
Devin Mork <devin.mork@gmail.com>
2021-02-12 08:50:39 -0600

    Initial commit.

---
+++ test_call.rb
@@ -0,0 +1,23 @@
+﻿require 'sinatra'
+require 'sinatra/multi_route'
+require 'json'
+
+before do
+  content_type :json
+end
+
+get '/' do
+    'Good news. It\'s working'
+  end
+
+route :get, :post, '/webhooks/answer' do
+  [{
+    action: 'connect',
+    endpoint: [{
+      type: 'phone',
+      number: '17635679484'
+    }]
+  }].to_json
+end
+
+set :port, 3000
