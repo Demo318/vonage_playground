fe5f0970dc98be5873699ef008a500793a41909b
Devin Mork <devin.mork@gmail.com>
2021-02-12 11:30:44 -0600

    fixed a bunch of stuff. works now.

--- inbound_voice_call.rb
+++ inbound_voice_call.rb
@@ -9,7 +9,7 @@
         @conversation_uuid = call_conversation_uuid
     end
 
-    def connect_voice_number(outbound_number)
+    def connect_voice_number(outbound_number, from_number)
         [{
             action: 'connect',
             endpoint: [{
@@ -17,6 +17,25 @@
                 number: outbound_number
                       }]
         }].to_json
+    end
+
+    def play_main_menu(base_url)
+        
+        [{
+            action: "talk",
+            text: "Welcome to the Devin Mork hotline. An entire hotline dedicated to Devin Mork. Please choos from one of the following options.",
+        },
+        {
+            action: "input",
+            type: [ 'dtmf' ],
+            dtmf: {
+                'maxDigits': 1,
+                'timeOut': 5
+            },
+            eventUrl: ["#{base_url}/webhooks/dtmf"]
+        }].to_json
+
+
     end
 
 end
--- main.rb
+++ main.rb
@@ -11,6 +11,12 @@ require '.\inbound_voice_call.rb'
 # Phone numbers, private credentials kept in separate class files, added to  .gitignore for privacy.
 require '.\phone_numbers.rb'
 
+helpers do
+  def parsed_body
+    JSON.parse(request.body.read)
+  end
+end
+
 
 before do
   content_type :json
@@ -20,17 +26,39 @@ numbers = PhoneNumbersList.new
 
 get '/' do
     'Good news. It\'s working'
-  end
+end
 
-get '/webhooks/answer' do
+route :get, :post, '/webhooks/answer' do
+
+  puts "the params are " + params.to_s
+  puts "the answer params are " + params["answer"].to_s
+
+
   call = InboundVoiceCall.new(params[:to],
                               params[:from],
                               params[:uuid],
                               params[:conversation_uuid]
                              )
 
+
+
+ call.play_main_menu(request.base_url)
   
-  
+end
+
+route :get, :post, '/webhooks/dtmf' do
+  dtmf = params['dtmf'] || parsed_body['dtmf']
+
+  puts "params are " + params.to_s if params
+  puts "dtmf is " + dtmf.to_s if dtmf
+
+
+  [{
+    action: 'talk',
+    text: "You pressed #{dtmf['digits']}"
+  }].to_json
+
+
 end
 
 set :port, 3000
